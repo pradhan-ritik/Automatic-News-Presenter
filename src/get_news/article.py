0da854193898c4d3682f9bb7e25bf7584894c5be
ritik <flipit001@gmail.com>
2024-08-04 18:30:50 -0700

    changed article class to dataclass instead of namedtuple.

--- src/get_news/article.py
+++ src/get_news/article.py
@@ -1,3 +1,17 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
-Article = namedtuple("Article", "title body link")
+@dataclass
+class Article:
+    title: str
+    body: str
+    link: str
+
+    # this is to be able to quickly identify duplicates in sets before wasting a bunch of time loading a webpage
+    def __hash__(self) -> int: 
+        return hash(self.title)
+
+    def __eq__(self, other: "Article") -> bool:
+        return self.title == other.title
+
+    def __ne__(self, other: "Article") -> bool:
+        return not self.__eq__(other)
