2d980e323700d80ee2edd5a5774a1c034c910ed1
ritik <flipit001@gmail.com>
2024-09-22 20:03:48 -0700

    changed str() to .to_text since this is better.

--- src/summarize_news/summarize_articles.py
+++ src/summarize_news/summarize_articles.py
@@ -2,4 +2,4 @@ from transformers import pipeline
 
 def summarize_article(article) -> str:
     model = pipeline("summarization", model="Falconsai/text_summarization")
-    return model(str(article), max_length=500, min_length=100)
+    return model(article.to_text(), max_length=500, min_length=100)
